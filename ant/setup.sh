fb468b56ffb5c861200cb0013f47364538c4baa1
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-02-27 21:18:03 +0100

    better WS

--- ant/setup.sh
+++ ant/setup.sh
@@ -1,11 +1,30 @@
 source build-global.properties
 source build-local.properties
 
-if [[ ! "x" = "x${ant_home}" ]]; then
-  export ANT_HOME="${ant_home}"
+source build-global.properties
+source build-local.properties
+
+if [[ "x" = "x${ANT_HOME}" ]]; then
+  export ANT_HOME="${ant}"
   export PATH="${ANT_HOME}/bin:${PATH}"
   fi
-if [[ ! "x" = "x${java_home}" ]]; then
-  export JAVA_HOME="${java_home}"
-  export PATH="${JAVA_HOME}/bin:${PATH}"
+if [[ ! "x" = "x${graphviz}" ]]; then
+  export PATH="${graphviz}/bin:${PATH}"
   fi
+for JAR in ../extlib/*.jar; do
+  export CLASSPATH=${CLASSPATH}:${JAR} 
+  done
+  
+export janusgraph_dir
+export zookeeper
+export hbase_table
+
+export home=`pwd`/..
+
+alias gremlin='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i "${jhtools_src}/gremlin/start.gremlin" -i ../src/gremlin/start2.gremlin'
+alias gremlin_local='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i ${jhtools_src}/gremlin/start_local.gremlin ${zookeeper} ${hbase_table} ${home} -i ../src/gremlin/start2_local.gremlin'
+alias phoenix_cli='${phoenix_cli}'
+
+echo "commands: gremlin, gremlin_local"
+
+
---
+++ src/ws/Servers.jsp
@@ -0,0 +1,2 @@
+<option value="http://134.158.74.85:24444">http://134.158.74.85:24444</option>
+<option value="http://localhost:8182">http://localhost:8182</option>
