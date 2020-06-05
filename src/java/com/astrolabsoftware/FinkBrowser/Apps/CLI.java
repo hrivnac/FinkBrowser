80ee793bd0264bace36660543d53ce3f5caf0d69
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-06-05 21:24:56 +0200

    better GUI

--- src/java/com/astrolabsoftware/FinkBrowser/Apps/CLI.java
+++ src/java/com/astrolabsoftware/FinkBrowser/Apps/CLI.java
@@ -4,10 +4,14 @@ import com.astrolabsoftware.FinkBrowser.Utils.Init;
 
 // Lomikel
 import com.Lomikel.Utils.Info;
+import com.Lomikel.GUI.Icons;
 
 // Bean Shell
 import bsh.Interpreter;
 import bsh.EvalError;
+
+// Swing
+import javax.swing.ImageIcon;
 
 // Log4J
 import org.apache.log4j.Logger;
@@ -24,9 +28,13 @@ import org.apache.log4j.Logger;
 public class CLI extends com.Lomikel.Apps.CLI {
   
   /** Start {@link Interpreter} and run forever.
-    * @param msg The message so show. */
-  public CLI(String msg) {
-    super(msg);
+    * @param icon      The {@ImageIcon} for menu.
+    * @param toolTiple The menu tooltip.
+    * @param msg       The message so show. */
+  public CLI(ImageIcon icon,
+             String    toolTip,
+             String    msg) {
+    super(icon, toolTip, msg);
     }
     
   @Override
@@ -46,7 +54,9 @@ public class CLI extends com.Lomikel.Apps.CLI {
   public static void main(String[] args) {
     Init.init();
     parseArgs(args, "java -jar FinkBrowser.exe.jar");
-    new CLI("Welcome to Fink Browser CLI " + Info.release() + "\nhttps://astrolabsoftware.github.io\n");
+    new CLI(Icons.icon("com/astrolabsoftware/FinkBrowser/Apps/images/Fink.png"),
+            "<html><h3>https://astrolabsoftware.github.io</h3></html>",
+            "Welcome to Fink Browser CLI " + Info.release() + "\nhttps://astrolabsoftware.github.io\n");
     }
     
   /** Logging . */
