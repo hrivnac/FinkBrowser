a539fc7882b35dc3b5c596ffda851d35b9ef0e38
Julius Hrivnac <Julius.Hrivnac@cern.ch>
2020-06-30 14:20:47 +0000

    all Fink graphs

--- src/sys/janusgraph/scripts/setup-env.groovy
+++ src/sys/janusgraph/scripts/setup-env.groovy
@@ -5,5 +5,11 @@ globals << [hook : [
   onShutDown: { ctx -> ctx.logger.info("Executed once at shutdown of Gremlin Server.")}
   ] as LifeCycleHook]
   
-globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "134.158.74.54").set("storage.hbase.table", "janusgraph").open()]
+//globals << [graph : JanusGraphFactory.build().set("storage.backend", "hbase").set("storage.hostname", "134.158.74.54").set("storage.hbase.table", "janusgraph").open()]
 globals << [g : graph.traversal()]
+globals << [g1 : graph1.traversal()]
+globals << [gTestJanusGraph : graphTestJanusGraph.traversal()]
+globals << [gTestJanusGraph2 : graphTestJanusGraph2.traversal()]
+globals << [gTestJGraph2 : graphTestJGraph2.traversal()]
+globals << [gTestJGraph3 : graphTestJGraph3.traversal()]
+//globals << [gTestJGraph4 : graphTestJGraph4.traversal()]
