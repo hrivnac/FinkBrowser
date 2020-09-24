9ee16e915aaab668b73f1e18ff98d2e6e06b5a1d
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-09-24 20:30:00 +0200

    better graph visualisation

--- src/ws/Stylesheet.js
+++ src/ws/Stylesheet.js
@@ -1,3 +1,19 @@
+stylesheet.nodes.AstroLabNet = {
+  graphics: {
+    label:"AstroLabNet", 
+    title:"AstroLabNet",        
+    subtitle:" ",     
+    group:" ",        
+    shape:"image",      
+    image:"AstroLab.png",        
+    borderRadius:"0", 
+    borderWidth:"1",  
+    borderDashes:[1,0],
+    value:"0"         
+    },
+  actions:[
+    ]
+  }
 stylesheet.nodes.site = {
   graphics: {
     label:"title", 
@@ -31,27 +47,11 @@ stylesheet.nodes.site = {
     {name:"HBase test tiny 3",  url:{gremlin:"properties('HBaseTestTiny3').value()"}, embedded:true}
     ]
   }
-stylesheet.nodes.AstroLabNet = {
+stylesheet.nodes.alert = {
   graphics: {
-    label:{gremlin:"label()"}, 
-    title:" ",        
-    subtitle:" ",     
-    group:" ",        
-    shape:"image",      
-    image:"AstroLab.png",        
-    borderRadius:"0", 
-    borderWidth:"1",  
-    borderDashes:[1,0],
-    value:"0"         
-    },
-  actions:[
-    ]
-  }
-stylesheet.nodes.vertex = {
-  graphics: {
-    label:"objectId",        
-    title:{gremlin:"sideEffect(values('classtar').store('1')).sideEffect(values('rfscore').store('2')).sideEffect(values('snnscore').store('3')).sideEffect(values('cdxmatch').store('4')).sideEffect(values('mulens_class_1').store('5')).sideEffect(values('mulens_class_2').store('6')).sideEffect(values('roid').store('7')).cap('1', '2', '3', '4', '5', '6', '7').next().values().join().replaceFirst('\\\\[', '<br>classtar=').replaceFirst('\\\\[', 'rfscore=').replaceFirst('\\\\[', 'snscore=').replaceFirst('\\\\[', 'dxmatch=').replaceFirst('\\\\[', 'mulens_class_1=').replaceFirst('\\\\[', 'mulens_class_2=').replaceFirst('\\\\[', 'roid=').replace(']', '<br>')"},
-    subtitle:{gremlin:"outE('similarity').count().join().toString().concat(' similar vertexes')"},
+    label:"rowkey",
+    title:"rowkey",        
+    subtitle:{gremlin:"sideEffect(values('classtar').store('1')).sideEffect(values('rfscore').store('2')).sideEffect(values('snnscore').store('3')).sideEffect(values('cdxmatch').store('4')).sideEffect(values('mulens_class_1').store('5')).sideEffect(values('mulens_class_2').store('6')).sideEffect(values('roid').store('7')).cap('1', '2', '3', '4', '5', '6', '7').next().values().join().replaceFirst('\\\\[', '<br>classtar=').replaceFirst('\\\\[', 'rfscore=').replaceFirst('\\\\[', 'snscore=').replaceFirst('\\\\[', 'dxmatch=').replaceFirst('\\\\[', 'mulens_class_1=').replaceFirst('\\\\[', 'mulens_class_2=').replaceFirst('\\\\[', 'roid=').replace(']', '<br>')"},
     group:" ",        
     shape:"image",      
     image:"Alert.png",        
@@ -63,14 +63,14 @@ stylesheet.nodes.vertex = {
   actions:[
     ]
   }
-stylesheet.nodes.Alert = {
+stylesheet.nodes.similarity = {
   graphics: {
-    label:"title", 
-    title:"title",        
-    subtitle:" ",
+    label:"recipe",
+    title:"recipe",        
+    subtitle:{gremlin:"sideEffect(values('recipe').store('1')).sideEffect(values('equals').store('2')).cap('1', '2').next().values().join().replaceFirst('\\\\[', '<br>recipe=').replaceFirst('\\\\[', 'equals=').replace(']', '<br>')"},
     group:" ",        
     shape:"image",      
-    image:"Alert.png",        
+    image:"Similarity.png",        
     borderRadius:"0", 
     borderWidth:"1",  
     borderDashes:[1,0],
@@ -96,42 +96,63 @@ stylesheet.nodes.AlertsCollection = {
     {name:"Latest Alerts",  url:{gremlin:"properties('hbase').value()"}, embedded:true}
     ]
   }
-stylesheet.edges.is = {
+stylesheet.edges.has = {
   graphics: {
     label:" ",
     title:" ",
     subtitle:" ",
     arrows:{to:{enabled:true, type:"vee"}},
-    value:"0.1",
-    group:"is"
+    value: "0.1",
+    group: " "
     },
   actions: [
-    {name:"info", url:"https://astrolabsoftware.github.io"}
     ]
   }
-stylesheet.edges.knows = {
+stylesheet.edges.holds = {
   graphics: {
     label:" ",
     title:" ",
     subtitle:" ",
-    arrows:{middle:{enabled:true, type:"arrow"}},
-    value:"0.1",
-    group:"knows"
+    arrows:{to:{enabled:true, type:"vee"}},
+    value: "0.1",
+    group: " "
     },
   actions: [
-    {name:"info", url:"https://astrolabsoftware.github.io"}
     ]
   }
-stylesheet.edges.similarity = {
+stylesheet.edges.contains = {
   graphics: {
-    label:"similarity",
-    title:"similarity",
+    label:" ",
+    title:" ",
     subtitle:" ",
-    arrows:{middle:{enabled:false, type:"arrow"}},
-    value:"similarity",
-    group:"similarity"
+    arrows:{to:{enabled:true, type:"vee"}},
+    value: "0.1",
+    group: " "
     },
   actions: [
-    {name:"info", url:"https://astrolabsoftware.github.io"}
+    ]
+  }
+stylesheet.edges.satr = {
+  graphics: {
+    label:{gremlin:"properties('weight').next().value().toString()"},
+    title:{gremlin:"properties('weight').next().value().toString()"},
+    subtitle:" ",
+    arrows:{to:{enabled:true, type:"vee"}},
+    value:"0.1",
+    group:{gremlin:"properties('weight').next().value().toString()"}
+    },
+  actions: [
+    ]
+  }
+stylesheet.edges.exactmatch = {
+  graphics: {
+    label:{gremlin:"properties('propertyname').next().value().toString()"},
+    title:{gremlin:"properties('propertyname').next().value().toString()"},
+    subtitle:" ",
+    arrows:{to:{enabled:false, type:"vee"}},
+    value: "0.1",
+    group: " "
+    },
+  actions: [
     ]
   }
