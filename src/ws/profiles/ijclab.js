36a7b28a3ea86ad280eb231d94c487afff25a996
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-09-29 16:32:46 +0200

    histoview fixes

--- src/ws/profiles/ijclab.js
+++ src/ws/profiles/ijclab.js
@@ -1,2 +1,2 @@
 bootstrap('text', 1);
-loadPane('result', 'HBaseTable.jsp?hbase=http://134.158.74.54:2181&htable=test_portal_tiny.3&group=i:objectId&schema=schema_0.7.0_0.3.6&latest=1000000&limit=10');
+loadPane('result', 'HBaseTable.jsp?hbase=http://134.158.74.54:2181&htable=test_portal_tiny.3&group=i:objectId&schema=schema_0.7.0_0.3.6&latest=1000000&limit=100');
