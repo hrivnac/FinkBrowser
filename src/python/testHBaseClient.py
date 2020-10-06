42146ffbb6e9dd7bacdfdb8b5eb7def22ec5a6e6
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-10-06 13:11:08 +0200

    Jython example

--- src/python/testHBaseClient.py
+++ src/python/testHBaseClient.py
@@ -7,15 +7,12 @@ from jpype import JImplements, JOverride, JImplementationFor
 # ../dist/FinkBrowser.exe.jar
 jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", "-Djava.class.path=" + sys.argv[1], convertStrings=False)
 
-import java.lang
-import java.util
-
-import com.Lomikel.HBaser
+from com.Lomikel.HBaser import HBaseClient
 
 true  = jpype.java.lang.Boolean(True)
 false = jpype.java.lang.Boolean(False)
 
-client = com.Lomikel.HBaser.HBaseClient("localhost", 2181)
+client = HBaseClient("localhost", 2181)
 client.connect("test_portal_tiny.3", "schema_0.7.0_0.3.6")
 client.setLimit(10)
 
