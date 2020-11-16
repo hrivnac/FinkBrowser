724785fbf94e0adf2705c9e9b1c2f310c32c070d
Julius Hrivnac <julius.hrivnac@cern.ch>
2020-11-16 20:44:36 +0100

    evolution plot with julian date

--- src/java/com/astrolabsoftware/FinkBrowser/WebService/FinkHBaseColumnsProcessor.java
+++ src/java/com/astrolabsoftware/FinkBrowser/WebService/FinkHBaseColumnsProcessor.java
@@ -19,20 +19,22 @@ import org.apache.log4j.Logger;
 public class FinkHBaseColumnsProcessor extends HBaseColumnsProcessor {
   
   /** Give the value of the X-axes (=Julian date) corresponding to one table row.
-    * @param entry0 One row of the table.
-    * @return       The derived value of x-axes. */
+    * @param entry One row of the table.
+    * @return      The derived value of x-axes. */
   @Override
-  public String getX(Map.Entry<String, Map<String, String>> entry0) {
-    //return entry0.getKey().split("_")[1];
-    return entry0.getValue().get("i:jd");
+  public String getTimestamp(Map<String, String> entry) {
+    String date = getDate(entry);
+    log.info(date + " -> " + Long.toString(DateTimeManagement.string2time(date, "yyyy MM dd HH:mm:ss.SSS")));
+    return Long.toString(DateTimeManagement.string2time(date, "yyyy MM dd HH:mm:ss.SSS"));
     }
   
   /** Give the date (from Julian date) corresponding to one table row.
-    * @param entry0 One row of the table.
-    * @return       The date (may not correspond to the row timestamp). */
+    * @param entry One row of the table.
+    * @return      The date (may not correspond to the row timestamp). */
   @Override
-  public String getXDate(Map.Entry<String, Map<String, String>> entry0) {
-    String days = getX(entry0);
+  public String getDate(Map<String, String> entry) {
+    String days = entry.get("i:jd");
+    log.info(days + " ->" + DateTimeManagement.julianDate2String(Double.valueOf(days), "yyyy MM dd HH:mm:ss.nnnnnnnnn"));
     return DateTimeManagement.julianDate2String(Double.valueOf(days), "yyyy MM dd HH:mm:ss.nnnnnnnnn");
     }
 
--- src/ws/Params.jsp
+++ src/ws/Params.jsp
@@ -1,6 +1,9 @@
+<%@ page import="com.Lomikel.WebService.HBase2Table" %>
 <%@ page import="com.astrolabsoftware.FinkBrowser.HBaser.FinkHBaseClient" %>
+<%@ page import="com.astrolabsoftware.FinkBrowser.WebService.FinkHBaseColumnsProcessor" %>
 
 <%
   String hbaseRowName = "alert";
   String hbaseRowKey  = "rowkey";
+  HBase2Table.changeColumnsProcessor(new FinkHBaseColumnsProcessor());
   %>
