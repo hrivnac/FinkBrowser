376bc9d200ad3dd9b1582b62c774e802499f6d73
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-03-01 10:33:16 +0100

    better janus index creation

---
+++ src/sh/initJanus.sh
@@ -0,0 +1,6 @@
+cd ../ant
+source setup.sh
+gremlin_console < ../src/gremlin/schema.gremlin
+gremlin_console < ../src/gremlin/indexHB.gremlin
+gremlin_console < ../src/gremlin/indexES.gremlin
+gremlin_console < ../src/gremlin/astrolabnet-${target}.gremlin
