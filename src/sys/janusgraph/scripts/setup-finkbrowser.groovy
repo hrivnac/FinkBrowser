49a4305a724ee67e34cbfdfd7e24506a0658d450
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-04-16 10:06:34 +0200

    ...

--- src/sys/janusgraph/scripts/setup-finkbrowser.groovy
+++ src/sys/janusgraph/scripts/setup-finkbrowser.groovy
@@ -3,3 +3,8 @@ clientH.connect("@STORAGE.HBASE.TABLE@", "@STORAGE.HBASE.SCHEMA@")
 com.Lomikel.Januser.Hertex.setHBaseClient(clientH)
 
 com.Lomikel.HBaser.HBaseClient.registerVertexType("alert", com.astrolabsoftware.FinkBrowser.Januser.Alert.class)
+
+def hi_finkbrowser() {
+  return "Hello World from FinkBrowser !"
+  }
+  
