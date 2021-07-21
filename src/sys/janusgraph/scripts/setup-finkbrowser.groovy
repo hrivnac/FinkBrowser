6dc8fb6fd2b2736624acbf48a11960f42a52b984
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-07-21 10:40:00 +0200

    server functions

--- src/sys/janusgraph/scripts/setup-finkbrowser.groovy
+++ src/sys/janusgraph/scripts/setup-finkbrowser.groovy
@@ -7,3 +7,13 @@ com.Lomikel.HBaser.HBaseClient.registerVertexType("alert", com.astrolabsoftware.
 def hi_finkbrowser() {
   return "Hello World from FinkBrowser !"
   }
+
+def geosearch(lat, lon, radius, jdmin, jdmax, limit) {
+  nDir = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, radius*6371.0087714*180/Math.PI))).count().next()
+  nJD  = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, radius*6371.0087714*180/Math.PI))).limit(nDir).has('jd', inside(jdmin, jdmax)).count().next()
+  if (limit < nJD) {
+    nJD = limit
+    }
+  return g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, radius*6371.0087714*180/Math.PI))).limit(nDir).has('jd', inside(jdmin, jdmax)).limit(nJD)
+  }
+  
