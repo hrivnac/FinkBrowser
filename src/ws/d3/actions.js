d3661538d2f9b513f368ae223345a62108bd2323
Julius Hrivnac <julius.hrivnac@cern.ch>
2021-08-31 16:31:10 +0200

    fixes

--- src/ws/d3/actions.js
+++ src/ws/d3/actions.js
@@ -1,5 +1,3 @@
 function actions(url, key) {
-  return "<a href='#' onclick='loadPane(\"result\", \"" + url + "&key="             + key               + "\")'        title='see details of this concrete alert'                            >alert</a>  - " +
-         "<a href='#' onclick='loadPane(\"result\", \"" + url + "&filters=key:key:" + key.split("_")[0] + ":prefix\")' title='see details of all alerts of the same objectId'                >alerts</a> - " +
-         "<a href='http://134.158.75.151:24000/"                                    + key.split("_")[0] + "'           title='analyse with Fink Scuence Portal'               target='_blank'>Analyse&#8599;</a>";
+  return "<a href='http://134.158.75.151:24000/" + key + "' title='analyse with Fink Science Portal' target='_blank'>Analyse&#8599;</a>";
   }
