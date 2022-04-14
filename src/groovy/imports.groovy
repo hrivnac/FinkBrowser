5f7ca4ad8402a6a3f64a66ea3c60be5c96da2300
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-04-14 13:18:38 +0200

    ...

--- src/groovy/imports.groovy
+++ src/groovy/imports.groovy
@@ -1,4 +1,3 @@
 import com.Lomikel.Januser.StringGremlinClient;
 StringGremlinClient client = new StringGremlinClient("134.158.74.85", 24444);
-print(client.interpret("importStatus()"));
-System.exit(0);
+client.interpret("importStatus()");
