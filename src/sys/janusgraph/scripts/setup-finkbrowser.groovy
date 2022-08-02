94505933b1b15859a652fef1eba22413c851ed71
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-08-02 13:22:07 +0200

    ...

--- src/sys/janusgraph/scripts/setup-finkbrowser.groovy
+++ src/sys/janusgraph/scripts/setup-finkbrowser.groovy
@@ -3,8 +3,7 @@ class FinkBrowserIJCLab {
   def static init() {
     clientH = new com.Lomikel.HBaser.HBaseClient("@STORAGE.HOSTNAME@", @STORAGE.HBASE.PORT@)
     clientH.connect("@STORAGE.HBASE.TABLE@", "@STORAGE.HBASE.SCHEMA@")
-    com.Lomikel.Januser.Hertex.setHBaseClient(clientH)
-    
+    com.Lomikel.Januser.Hertex.setHBaseClient(clientH)    
     com.Lomikel.HBaser.HBaseClient.registerVertexType("alert", com.astrolabsoftware.FinkBrowser.Januser.Alert.class)
     println "class FinkBrowser IJCLab initialised"
     }
@@ -14,15 +13,15 @@ class FinkBrowserIJCLab {
     }
   
   def static geosearch_help() {
-    return 'geosearch(ra, dec, ang[deg], jdmin, jdmax, limit)'
+    return 'geosearch(g, ra, dec, ang[deg], jdmin, jdmax, limit)'
     }
     
-  def static geosearch(ra, dec, ang, jdmin, jdmax, limit) {
-    lat = dec
-    lon = ra - 180
-    dist = ang * 6371.0087714 * Math.PI / 180
-    nDir = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, dist))).count().next()
-    nJD  = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, dist))).limit(nDir).has('jd', inside(jdmin, jdmax)).count().next()
+  def static geosearch(g, ra, dec, ang, jdmin, jdmax, limit) {
+    def lat = dec
+    def lon = ra - 180
+    def dist = ang * 6371.0087714 * Math.PI / 180
+    def nDir = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, dist))).count().next()
+    def nJD  = g.V().has('direction', geoWithin(Geoshape.circle(lat, lon, dist))).limit(nDir).has('jd', inside(jdmin, jdmax)).count().next()
     if (limit < nJD) {
       nJD = limit
       }
@@ -33,25 +32,26 @@ class FinkBrowserIJCLab {
     return 'drop_by_date(graph, importDate, nCommit, tWait)'
     }
     
-  def static drop_by_date(importDate, nCommit, tWait) {
+  def static drop_by_date(graph, importDate, nCommit, tWait) {
+    def g = graph.traversal()
     //importDate = 'Mon Feb 14 05:51:20 UTC 2022'
     //nCommit = 500
-    i = 0
-    tot = 0
-    nMax=g.V().has('importDate', importDate).count().next()
+    def i = 0
+    def tot = 0
+    def nMax = g.V().has('importDate', importDate).count().next()
     println('' + nMax + ' vertexes to drop')
-    t0 = System.currentTimeMillis()
+    def t0 = System.currentTimeMillis()
     while(true) {
       g.V().has('importDate', importDate).limit(nCommit).out().out().drop().iterate()
       g.V().has('importDate', importDate).limit(nCommit).out().drop().iterate()
       g.V().has('importDate', importDate).limit(nCommit).drop().iterate()
       graph.traversal().tx().commit()
       Thread.sleep(tWait)
-      tot = nCommit * ++i
-      dt = (System.currentTimeMillis() - t0) / 1000
-      per = 100 * tot / nMax
-      freq = tot / dt
-      rest = (nMax - tot) / freq / 60 /60
+      def tot = nCommit * ++i
+      def dt = (System.currentTimeMillis() - t0) / 1000
+      def per = 100 * tot / nMax
+      def freq = tot / dt
+      def rest = (nMax - tot) / freq / 60 /60
       println(tot + ' = ' + per + '% at ' + freq + 'Hz, ' + rest + 'h to go')
       }
     }
@@ -60,8 +60,8 @@ class FinkBrowserIJCLab {
     return 'importStatus(g)'
     }
       
-  def static importStatus() {
-    txt = ''
+  def static importStatus(g) {
+    def txt = ''
     txt += 'Imported:\n'
     g.V().has('lbl', 'Import').has('nAlerts', neq(0)).order().by('importSource').valueMap('importSource', 'importDate', 'nAlerts').each{txt += '\t' + it + '\n'}
     txt += 'Importing:\n'
@@ -70,10 +70,10 @@ class FinkBrowserIJCLab {
     }
     
   def static candidates_help() {
-    return 'candidates(objectId)';
+    return 'candidates(g, objectId)';
     }
     
-  def static candidates(objectId) {
+  def static candidates(g, objectId) {
     return g.V().has('objectId', objectId).out().has('lbl', 'candidate');
     }  
     
