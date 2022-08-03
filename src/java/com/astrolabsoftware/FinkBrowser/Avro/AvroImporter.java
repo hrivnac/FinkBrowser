f5a45c4430e9baf40b8e96e7a5f353cdb77087af
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-08-03 13:51:46 +0200

    ...

--- src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
+++ src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
@@ -190,7 +190,6 @@ public class AvroImporter extends JanusClient {
     if (_top) {
       _topFn = fn;
       now();
-      log.info(g().V().limit(1).next());
       Vertex import1 = g().addV("Import").property("lbl", "Import").property("importSource", fn).property("importDate", _date).next();
       Vertex imports = g().V().has("lbl", "site").has("title", "IJCLab").out().has("lbl", "Imports").next();
       _gr.addEdge(imports, import1, "has"); 
