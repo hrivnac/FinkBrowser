6b71830adb2edf91bc2fb53a86800899a2df5b6d
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-08-04 11:37:41 +0200

    ...

--- ant/setup.sh
+++ ant/setup.sh
@@ -11,7 +11,7 @@ if [[ ! "x" = "x${graphviz}" ]]; then
 for JAR in ../extlib/*.jar; do
   export CLASSPATH=${CLASSPATH}:${JAR} 
   done
-export GREMLIN_CLASSPATH="${groovy_sql_jar}:`pwd`/${healpix_jar}:${janusgraph_dir}/*.jar"  
+export GREMLIN_CLASSPATH="${groovy_sql_jar}:`pwd`/${healpix_jar}"  
 
 export janusgraph_dir
 export zookeeper
@@ -19,8 +19,8 @@ export hbase_table
 
 export home=`pwd`/..
 
-alias gremlin_Local='${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_Local.gremlin"                                                                  -i "../src/gremlin/start2.gremlin"'
-alias gremlin_IJCLab='${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_IJCLab.gremlin"                                                                 -i "../src/gremlin/start2.gremlin"'
+alias gremlin_Local='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_Local.gremlin"                                                                  -i "../src/gremlin/start2.gremlin"'
+alias gremlin_IJCLab='CLASSPATH="" ${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_IJCLab.gremlin"                                                                 -i "../src/gremlin/start2.gremlin"'
 alias gremlin_console='CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local.properties ${home}" -i "../src/gremlin/start2_console.gremlin"'
 
 
