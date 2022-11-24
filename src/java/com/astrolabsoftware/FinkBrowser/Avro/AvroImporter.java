cf7ee9a845c36c977abe3cabad634660cd4a3c7e
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-11-24 12:02:37 +0100

    ...

--- src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
+++ src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
@@ -326,7 +326,7 @@ public class AvroImporter extends JanusClient {
       Array<Double> array = (Array<Double>)(record.get("pca"));
       Iterator<Double> it = array.iterator();
       while (it.hasNext()) {
-        v.property(propertyName, it.next());
+        v.property("pca", it.next());
         }
       v.property("importDate", _date);      
       }
