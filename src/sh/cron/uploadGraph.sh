2b6dab380f100a97090eda324089838ea03bcf12
Julius Hrivnac <julius.hrivnac@cern.ch>
2022-11-30 10:14:40 +0100

    ...

--- src/sh/cron/uploadGraph.sh
+++ src/sh/cron/uploadGraph.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash -l
-D=${1}
-LOG=/tmp/upload202109${D}
+M=${1}
+D=${2}
+LOG=/tmp/upload2021${M}${D}
+/bin/rm -f ${LOG}
 cd ~/FinkBrowser/ant
 source ./setup.sh
 for X in 0 1 2 3 4 5 6 7 8 9; do
   /bin/rm -f ${LOG}${X}.log
-  ../src/sh/importAvroHDFS-IJCLab.sh /user/julien.peloton/archive_avro/science/year=2021/month=09/day=${D}${X} > ${LOG}${X}.log 2>&1
+  ../src/sh/importAvroHDFS-IJCLab.sh /user/julien.peloton/archive_avro/science/year=2021/month=${M}/day=${D}${X} > ${LOG}${X}.log 2>&1
   done
