b2c773dc7dfe9c3e7ab07753a7e888c2f0b6b170
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-01-24 11:01:57 +0100

    ...

--- src/sh/cron/processGraph.sh
+++ src/sh/cron/processGraph.sh
@@ -3,4 +3,4 @@ F=${1}
 LOG=/tmp/process.`basename ${F}`.log
 cd ~/FinkBrowser/ant
 source ./setup.sh
-CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" -i "../src/gremlin/start2_console.gremlin" < ${F} | ${LOG} 2>&1
+CLASSPATH="${GREMLIN_CLASSPATH}" ${janusgraph_dir}/bin/gremlin.sh -i "${lomikel_src}/gremlin/start_console.gremlin ${janusgraph_dir}/conf/gremlin-server/Local-IJCLab.properties ${home}" -i "../src/gremlin/start2_console.gremlin" < ${F} | tee ${LOG} 2>&1
