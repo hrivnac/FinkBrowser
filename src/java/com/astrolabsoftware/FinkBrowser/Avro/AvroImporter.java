52a6c488f06bf6122f0a837d36bd281015d15b51
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-07-06 17:21:46 +0200

    ...

--- src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
+++ src/java/com/astrolabsoftware/FinkBrowser/Avro/AvroImporter.java
@@ -265,7 +265,7 @@ public class AvroImporter extends JanusClient {
       String objectId = record.get("objectId").toString();
       //Vertex s = _gr.getOrCreate("source", "objectId", objectId).get(0); // TBD: check uniqueness
       Vertex s = vertex(record, "source", "objectId", "reuse");
-      //_gr.addEdge(s, v, "has");
+      _gr.addEdge(s, v, "has");
       for (Map.Entry<String, String> entry : values.entrySet()) {
         //log.debug("\t" + entry.getKey() + " = " + entry.getValue());
         try {
