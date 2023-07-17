e824b9dcbe18fffe7d54e9adef968d6f459a39a1
Julius Hrivnac <julius.hrivnac@cern.ch>
2023-07-17 18:48:45 +0200

    ...

--- src/sh/initJanus.sh
+++ src/sh/initJanus.sh
@@ -1,5 +1,10 @@
 cd ../ant
 source setup.sh
+curl -X DELETE 'http://127.0.0.1:20200/*'
+gremlin_console_Local < ${lomikel_dist}/src-${lomiklel_ver}/gremlin/clean.gremlin
+gremlin_console_Local < ${lomikel_dist}/src-${lomiklel_ver}/gremlin/schema.gremlin
+gremlin_console_Local < ${lomikel_dist}/src-${lomiklel_ver}/gremlin/indexHB.gremlin
+gremlin_console_Local < ${lomikel_dist}/src-${lomiklel_ver}/gremlin/indexES.gremlin
 gremlin_console_Local < ../src/gremlin/schema.gremlin
 gremlin_console_Local < ../src/gremlin/indexHB.gremlin
 gremlin_console_Local < ../src/gremlin/indexES.gremlin
